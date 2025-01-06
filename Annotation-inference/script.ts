127c2c31d18dfefde0ea5fcf9f1222f1ffdd39cd
LeonardoLobas <leonardolobas2703@gmail.com>
2025-01-06 15:52:13 -0300

    Adiciona cálculo de ganho e armazenamento de total no localStorage

---
+++ Annotation-inference/script.ts
@@ -0,0 +1,26 @@
+const input = document.querySelector("input");
+
+const total = localStorage.getItem("total");
+
+if (input && total) {
+    input.value = total;
+    calcularGanho(Number(input.value));
+}
+
+function calcularGanho(value: number) {
+    const p = document.querySelector("p");
+    if (p) {
+        p.innerText = `Seu ganho será de ${value + 100 - value * 0.2}`;
+    }
+}
+
+function salvarTotal() {
+    if (input) {
+        localStorage.setItem("total", input.value);
+        calcularGanho(Number(input.value));
+    }
+}
+
+if (input) {
+    input.addEventListener("keyup", salvarTotal);
+}
