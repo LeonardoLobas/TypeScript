8e3a91edf698c256c1d69ebda4ddcc1cc4f10cb1
LeonardoLobas <leonardolobas2703@gmail.com>
2025-01-08 08:31:20 -0300

    Adiciona arquivos HTML e JavaScript para exibir informações de um produto

---
+++ Types e Interfaces/script.ts
@@ -0,0 +1,26 @@
+interface Product { nome : string; preco: number; descricao: string; garantia: number; seguroAcidentes: boolean;  empresaFabricante:Empresa; empresaMontadora:Empresa; } 
+
+interface Empresa { fundacao: number; nome: string; pais: string; }
+
+
+async function fetchProduct() {
+    const response = await fetch('https://api.origamid.dev/json/notebook.json');
+    const product = await response.json();
+    console.log(product);
+    showProduct(product);
+}
+
+fetchProduct();
+
+function showProduct(data:Product) {
+    document.body.innerHTML = `    <div>
+      <h2>${data.nome}</h2>
+      <p>R$ ${data.preco}</p>
+      <div>
+        <h3>Fabricante: ${data.empresaFabricante.nome}</h3>
+      </div>
+      <div>
+        <h3>Montadora: ${data.empresaMontadora.nome}</h3>
+      </div>
+    </div>`;
+}
