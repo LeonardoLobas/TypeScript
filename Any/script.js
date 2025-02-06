99f2e9b0dc4630a075ee6f01f1f8cd6ef2c189ca
LeonardoLobas <leonardolobas2703@gmail.com>
2025-02-06 16:26:11 -0300

    Adiciona arquivos HTML e JavaScript para implementar a verificação de tipo com instanceof e remove código desnecessário

--- Any/script.js
+++ Any/script.js
@@ -1,14 +1,18 @@
 "use strict";
-function mostrarCursos(cursos) {
-    cursos.forEach((curso) => {
-        document.body.innerHTML += `
-        <div>
-          <h2>${curso.nome}</h2>
-          <p>Horas: ${curso.horas}</p>
-        </div>
-      `;
-    });
-}
-const dados = "o any gera problemas";
-mostrarCursos(dados);
-// NUNCA USAR ANY ---------------------------------------------------------
+// interface Curso {
+//     nome: string;
+//     horas: number;
+// }
+// function mostrarCursos(cursos: Curso[]) {
+//     cursos.forEach((curso) => {
+//         document.body.innerHTML += `
+//         <div>
+//           <h2>${curso.nome}</h2>
+//           <p>Horas: ${curso.horas}</p>
+//         </div>
+//       `;
+//     });
+// }
+// const dados: any = "o any gera problemas";
+// mostrarCursos(dados);
+// // NUNCA USAR ANY ---------------------------------------------------------
