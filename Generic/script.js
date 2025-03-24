e4a615840050c53363c6788d81c9a3001669c1eb
LeonardoLobas <leonardolobas2703@gmail.com>
2025-03-24 16:50:36 -0300

    Adiciona arquivos HTML, JavaScript e TypeScript para implementar a estrutura básica e a recuperação de dados de uma API

---
+++ Generic/script.js
@@ -0,0 +1,11 @@
+"use strict";
+const Link = document.querySelector(".link");
+Link?.href;
+async function getData(url) {
+    const response = await fetch(url);
+    return await response.json();
+}
+async function handleData() {
+    const notebook = await getData("https://api.origamid.dev/json/notebook.json");
+    console.log(notebook.nome);
+}
---
+++ Generic/script.ts
@@ -0,0 +1,18 @@
+const Link = document.querySelector<HTMLAnchorElement>(".link");
+Link?.href;
+
+async function getData<T>(url: string): Promise<T> {
+    const response = await fetch(url);
+    return await response.json();
+}
+
+interface Notebook {
+    nome: string;
+}
+
+async function handleData() {
+    const notebook = await getData<Notebook>(
+        "https://api.origamid.dev/json/notebook.json"
+    );
+    console.log(notebook.nome);
+}
