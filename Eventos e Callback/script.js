b92a17692edd8139b6be5079459da0f6109d61ae
LeonardoLobas <leonardolobas2703@gmail.com>
2025-03-24 10:07:41 -0300

    Adiciona arquivos HTML e JavaScript para implementar a ativação de menu com eventos de mouse e toque

---
+++ Eventos e Callback/script.js
@@ -0,0 +1,13 @@
+"use strict";
+function ativarMenu(event) {
+    console.log(event.type);
+    if (event instanceof MouseEvent) {
+        console.log(event.pageX);
+    }
+    if (event instanceof TouchEvent) {
+        console.log(event.touches[0].pageX);
+    }
+}
+document.documentElement.addEventListener("mousedown", ativarMenu);
+document.documentElement.addEventListener("touchstart", ativarMenu);
+document.documentElement.addEventListener("pointerdown", ativarMenu);
