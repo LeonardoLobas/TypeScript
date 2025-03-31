6abb05ca8b73341523813ba6425a82db9ea7676f
LeonardoLobas <leonardolobas2703@gmail.com>
2025-03-31 16:53:20 -0300

    Adiciona função para arredondar números em TypeScript

--- Function/script.ts
+++ Function/script.ts
@@ -8,3 +8,14 @@ somarNumeros(3, 4, 1);
 const subtrair = (a: number, b: number): number => a - b;
 
 subtrair(10, 2);
+
+function arredondar(valor: number | string): number | string {
+    if (typeof valor === "number") {
+        return Math.ceil(valor);
+    } else {
+        return Math.ceil(Number(valor)).toString();
+    }
+}
+
+console.log(arredondar(200.32));
+console.log(arredondar("200.32"));
